999b0d79dfef89b47541da127f5bffab739609e0
Shan Yu Lin <ineslin35@gmail.com>
2022-06-07 19:58:55 +0800

    fix: change the layout css in the Feature component

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -4,8 +4,10 @@ function Feature({ title, content, icon, isLinked }) {
     return (
         <div>
             <div className='relative mt-8 mx-auto pb-8 w-10/12 rounded bg-white'>
-                <div className='feature-icon-container flex justify-center items-center absolute w-20 h-20 rounded-full bg-dark_violet'>
-                    <img className='w-10' src={icon} />
+                <div className='feature-icon-container absolute w-full'>
+                    <div className='flex justify-center items-center mx-auto w-20 h-20 rounded-full bg-dark_violet'>
+                        <img className='w-10' src={icon} />
+                    </div>
                 </div>
                 <div className='pt-20 text-center'>
                     <h3 className='mb-4 text-xl font-bold'>{title}</h3>
@@ -13,8 +15,8 @@ function Feature({ title, content, icon, isLinked }) {
                 </div>
             </div>
             { isLinked &&
-                <div>
-                    <div className='feature-link-line w-2 h-12 bg-cyan'></div>
+                <div className='relative mx-auto w-10/12'>
+                    <div className='feature-link-line mx-auto w-2 h-12 bg-cyan'></div>
                 </div>
             }
         </div>
