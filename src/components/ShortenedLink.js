e179e48afc6dc4f4ed7227b3af2e6786fe8706eb
Shan Yu Lin <ineslin35@gmail.com>
2022-06-20 12:06:47 +0800

    chore: install React Router

--- src/components/ShortenedLink.js
+++ src/components/ShortenedLink.js
@@ -25,7 +25,7 @@ function ShortenedLink({ originUrl, shortenedUrl }) {
             <div className="my-3 w-full h-px bg-custom-gray"></div>
             <div className="px-4 break-words text-cyan">{shortenedUrl}</div>
             <div className="px-4">
-                <button onClick={copyTheLink} className={`mt-3 w-full h-14 ${buttonStyle} text-white rounded-lg xl:mt-0 xl:ml-6 xl:w-2/12`}>
+                <button onClick={copyTheLink} className={`mt-3 w-full h-14 ${buttonStyle} text-white rounded-lg`}>
                     {buttonText}
                 </button>
             </div>
